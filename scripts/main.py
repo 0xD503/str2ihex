fb5929635f09b9d5586b636aebdfdc021e16138d
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2022-11-23 18:51:24 +0100

    Refactored main script

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
-## TODO: add to str2ihex() hex/bin/oct representation support
+## TODO: add to str2ihex() hex/bin/oct/dec representation support
 import sys
 import argparse
 from intelhex import IntelHex
 
 
+def is_valid_number(string, encoding=16):
+    """Returns True if string is valid character-coded number.
+
+    Keyword arguments:
+    string -- character-coded number
+    encoding -- encoding scheme: 2 - bin, 8 - oct, 10 - dec, 16 - hex. Defaults to 16 (hex)
+    """
+    status = False
+    try:
+        int(string, encoding)
+        status = True
+    except ValueError as excpt:
+        print("ValueError EXCEPTION:", excpt)
+        sys.exit()
+    return status
+
 def read_lines(in_file):
-    """Reads file and returns list of its lines."""
+    """Reads file and returns list of its lines.
+
+    Keyword arguments:
+    in_file -- input file with character-coded numbers
+    """
     with open(in_file, 'r', encoding = "utf-8") as file:
         lines_lst = []
         for line in file:
@@ -15,7 +35,7 @@ def read_lines(in_file):
     return lines_lst
 
 def str2ihex(in_list):
-    """Takes a list of numbers in character-coded bin/hex/oct numbers
+    """Takes a list of numbers in character-coded bin/hex/oct/dec numbers
     representation and returns filled instance of IntelHex() formatter.
 
     Keyword arguments:
@@ -25,11 +45,8 @@ def str2ihex(in_list):
     words_num = int(str_len / 2)
     intel_hex = IntelHex()
     for idx, line in enumerate(in_list):
-        try:
-            intel_hex[idx * words_num:idx * words_num + words_num] = list(bytearray.fromhex(line))
-        except ValueError as excpt:
-            print("Invalid input value: ", excpt)
-            return None
+        is_valid_number(line, 16)
+        intel_hex[idx * words_num:idx * words_num + words_num] = list(bytearray.fromhex(line))
     return intel_hex
 
 
