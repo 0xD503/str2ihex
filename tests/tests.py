74fb636683ae2f416548b54385cbf6c4980a30f9
Ruslan Vostretsov <ElectricalBrain@protonmail.com>
2022-11-26 00:10:03 +0100

    Add copiright placeholders

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+## Copyright (c) 2022 Ruslan Vostretsov
+## All rights reserved.
+## This file is a part of project str2ihex which is released under BSD 3-Clause
+## license. Go to https://github.com/0xD503/str2ihex for full license details
+
+
 import unittest
 
 import os, sys
